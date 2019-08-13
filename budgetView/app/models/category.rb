931c101d62a8a2e0521477b5f83a984c0941ea14
Kenneth Ofosu <kofosu89@gmail.com>
2019-08-13 12:10:19 -0400

    add models and validations

--- budgetView/app/models/category.rb
+++ budgetView/app/models/category.rb
@@ -1,2 +1,13 @@
 class Category < ApplicationRecord
+	belongs_to :user
+	has_many :entries
+
+
+	validates :name, presence: true
+	validates :type, presence: true
+	validates :goal, numericality: true, presence: true
+	validates :current_total, numericality: true, presence: true
+
+	
+
 end
--- budgetView/app/models/entry.rb
+++ budgetView/app/models/entry.rb
@@ -1,2 +1,13 @@
 class Entry < ApplicationRecord
+	belongs_to :category
+
+
+
+	validates :name, presence: true
+	validates :description, presence: true
+	validates :date, presence: true
+	validates :amount, numericality: true, presence: true
+
+
+
 end
