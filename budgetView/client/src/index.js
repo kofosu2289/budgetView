118a75bcbcd464790549b6eb528575c6c656c738
Kenneth Ofosu <kofosu89@gmail.com>
2019-08-13 12:35:00 -0400

    create navbar react component baseplate

--- budgetView/client/src/index.js
+++ budgetView/client/src/index.js
@@ -1,10 +1,10 @@
 // Application entrypoint.
 
 // Load up the application styles
-require('../styles/application.scss');
+// require('../styles/application.scss');
 
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-
+import App from "./App";
 ReactDOM.render(<App />, document.getElementById("react-root"));
