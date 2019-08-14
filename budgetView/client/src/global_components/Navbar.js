2fe26d9a50ed73a4e7980682b9d39885045c30a4
Kenneth Ofosu <kofosu89@gmail.com>
2019-08-14 10:50:50 -0400

    message center button only appears when user is logged in

--- budgetView/client/src/global_components/Navbar.js
+++ budgetView/client/src/global_components/Navbar.js
@@ -13,11 +13,23 @@ class Navbar extends Component {
   }
 
   render() {
-    var user = localStorage.getItem('jwtToken')
+    const user = localStorage.getItem('jwtToken')
+    let button1
+    let button2
     if (user) {
-      var button = <NavLink to='/'><div className="navbar-right-div logout" onClick={this.logout}>Logout</div></NavLink>
+      button1 =
+      <NavLink to='/'>
+        <div className="navbar-right-div logout" onClick={this.logout}>Logout</div>
+      </NavLink>
+      button2 =
+      <NavLink to='/home'>
+        <div className="navbar-right-div">Message Center</div>
+      </NavLink>
     } else {
-      button = <NavLink to='/login'><div className="navbar-right-div login" onClick={this.login}>Login</div></NavLink>
+      button1 =
+      <NavLink to='/login'>
+        <div className="navbar-right-div login" onClick={this.login}>Login</div>
+      </NavLink>
     }
     return (
       <nav className="navbar justify-content-between mb-4">
@@ -25,10 +37,8 @@ class Navbar extends Component {
           <div className="navbar-brand">BudgetView</div>
         </NavLink>
         <div className="navbar justify-content-between">
-          <NavLink to='/home'>
-            <div className="navbar-right-div">Message Center</div>
-          </NavLink>
-          {button}
+          {button2}
+          {button1}
         </div>
       </nav>
     )
