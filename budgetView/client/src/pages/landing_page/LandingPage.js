1982a8a79e8152fc0196bc9a07c85404860dc4dc
Kenneth Ofosu <kofosu89@gmail.com>
2019-08-19 13:36:57 -0400

    modify landing page

--- budgetView/client/src/pages/landing_page/LandingPage.js
+++ budgetView/client/src/pages/landing_page/LandingPage.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import Content from "./components/Content";
-import LoginWithFacebook from "./components/FacebookLogin";
 
 class LandingPage extends Component {
   render() {
