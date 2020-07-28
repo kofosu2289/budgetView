623188fbc65fbf3939157a3adedc83a6e03062d6
Kenneth Ofosu <kofosu89@gmail.com>
2020-07-28 18:24:25 -0400

    add serviceWorker

--- budgetView/client/src/index.js
+++ budgetView/client/src/index.js
@@ -4,5 +4,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(<App />, document.getElementById("react-root"));
+
+serviceWorker.register();
